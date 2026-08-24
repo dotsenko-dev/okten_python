b32400f1fd20d399b0c0bf81c66bf6b39a0415dc
dotsenko-dev <roman.dotsenko.de@gmail.com>
2026-08-24 16:33:12 +0200

    HW2

--- HW2.py
+++ HW2.py
@@ -1,3 +1,4 @@
+"""
 BE. Python core. task 2
 
 1. написати функцію (notebook) на замикання, котра буде в собі зберігати список справ, вам потрібно реалізувати два методи:
@@ -18,7 +19,37 @@ BE. Python core. task 2
     return ... -->
 
 2. протипізувати перше завдання
+"""
 
+# from collections.abc import Callable
+
+# NotebookType = tuple[Callable[[str], None], Callable[[], list[str]]]
+
+
+# def notebook() -> NotebookType:
+#     todo_list: list[str] = []
+
+#     def add_todo(todo: str) -> None:
+#         todo_list.append(todo)
+
+#     def get_all() -> list[str]:
+#         return todo_list.copy()
+
+#     return add_todo, get_all
+
+
+# add_todo, get_all = notebook()
+
+# add_todo("Варення")
+# add_todo("Соус")
+
+# print(get_all())
+
+# add_todo("Перевірка")
+
+# print(get_all())
+
+"""
 3. створити функцію, котра буде повертати суму розрядів числа у вигляді строки (також використовуємо типізацію)
 
 Приклад:
@@ -28,7 +59,19 @@ expanded_form(12) # return ’10 + 2′
 expanded_form(42) # return ’40 + 2′
 
 expanded_form(70304) # return ‘70000 + 300 + 4’
+"""
 
+
+# def expanded_form(num: int) -> str:
+#     s = str(num)
+#     l = len(s)
+
+#     return ", ".join([d + "0" * (l - 1 - i) for i, d in enumerate(s) if d != "0"])
+
+
+# print(expanded_form(4078301))
+
+"""
 4. створити декоратор, котрий буде підраховувати, скільки разів була запущена функція, продекорована цим декоратором, та буде виводити це значення після виконання функцій
 
 <!-- @decor
@@ -43,3 +86,40 @@ func1()
 func1()
 func2()
 func1() -->
+"""
+
+# from collections.abc import Callable
+# from typing import Any
+
+
+# def decor(func: Callable[..., Any]) -> Callable[..., Any]:
+#     count = 0
+
+#     def inner(*args: Any, **kwargs: Any) -> Any:
+#         nonlocal count
+#         count += 1
+
+#         result = func(*args, **kwargs)
+#         print(f"{func.__name__} - {count}")
+
+#         return result
+
+#     return inner
+
+
+# @decor
+# def func_1() -> None:
+#     pass
+
+
+# @decor
+# def func_2() -> None:
+#     pass
+
+
+# func_1()
+# func_1()
+# func_2()
+# func_1()
+# func_1()
+# func_2()
